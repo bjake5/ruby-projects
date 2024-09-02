d7ce4f6f54c59bddafb93fb9cfa2662a721850da
Blake <21201344+bjake5@users.noreply.github.com>
2024-09-02 17:16:38 -0400

    Add initial mastermind logic

---
+++ mastermind/app/game_board.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative 'game_rules'
+require_relative 'player'
+require_relative 'guess'
+
+class GameBoard
+  include ActionText
+
+  def initialize; end
+end
